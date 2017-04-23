11e997759a1690d524e854dbcaaeae8a382d5ad8
Daniel Hawton <daniel@hawton.com>
2017-04-23 12:29:46 -0800

    standardize routing for easier parsing later

--- app/Console/Commands/UpdateVATSIM.php
+++ app/Console/Commands/UpdateVATSIM.php
@@ -125,8 +125,9 @@ class UpdateVATSIM extends Command
             $data[callsign] = str_replace("-", "", $data[callsign]); // Remove -'s from callsigns
             $data[planned_altitude] = preg_replace("/[FAL]+/", "", $data[planned_altitude]); // Change ICAO and FL350 altitude entries into VATSIM-standard
             if (strlen($data[planned_altitude]) < 4) $data[planned_altitude] = $data[planned_altitude] . "00"; // Make altitudes consistent
+            $data[planned_route] = str_replace("DCT", "", $data[planned_route]);
             $data[planned_route] = preg_replace("/\s+/", " ", $data[planned_route]); // Remove extra spaces
-            $data[planned_route] = preg_replace("/\+/", "", $data[planned_route]); // Ignore VATSIM ATC amendment markings
+            $data[planned_route] = preg_replace("/[\+\.]/", "", $data[planned_route]); // Ignore VATSIM ATC amendment and misc. markings
 
             $new = 0;
             $flight = Flight::where('callsign', $data[callsign])->where('vatsim_id', $data[cid])->orderBy("created_at")->first();
