ea9769e80c201f969b13a6161909c9be75944b01
Daniel Hawton <daniel@hawton.com>
2017-04-23 11:51:49 -0800

    Fix math helper

--- app/Classes/MathHelper.php
+++ app/Classes/MathHelper.php
@@ -12,8 +12,8 @@ namespace App\Classes;
 class MathHelper
 {
     public static function calc_distance($lat1, $lon1, $lat2, $lon2) {
-        $lat1 = deg2rad($lat1); $lon1 = deg2rad($lon1);
-        $lat2 = deg2rad($lat2); $lon2 = deg2rad($lon2);
+        $lat1 = deg2rad(floatval($lat1)); $lon1 = deg2rad(floatval($lon1));
+        $lat2 = deg2rad(floatval($lat2)); $lon2 = deg2rad(floatval($lon2));
 
         $theta = $lon1 - $lon2;
         $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
