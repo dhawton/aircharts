97df0420eb4d33f694957c718732b967bf8b0237
Daniel Hawton <daniel@hawton.com>
2017-04-24 13:28:19 -0800

    rewrite spider:pl

--- app/Console/Commands/SpiderPL.php
+++ app/Console/Commands/SpiderPL.php
@@ -23,7 +23,10 @@ class SpiderPL extends Command
      */
     protected $description = 'Spider for Poland charts (VATSIM)';
 
-    protected $index = "http://www.pl-vacc.org.pl/pol3/charts_vateud/index_aircharts.txt";
+    protected $base_url = "http://www.pl-vacc.org.pl/pol3/";
+    protected $index = "http://www.pl-vacc.org.pl/pol3/airports.php";
+    protected $chart_base = "http://www.pl-vacc.org.pl/files/maps/";
+
 
     /**
      * Create a new command instance.
@@ -43,32 +46,45 @@ class SpiderPL extends Command
     public function handle()
     {
         $data = file($this->index, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+        $airports = [];
         foreach($data as $line) {
-            $d = explode("|", $line);
-
-            if (strtoupper($d[CHART_TYPE]) == "PACKAGE") continue;
-
-            $chart = Chart::where('icao', $d[ICAO])->where('chartname', $d[CHART_NAME])->first();
-            if (!$chart) {
-                $chart = new Chart();
+            if (preg_match("!href=\"(airports\.php?d=EP..)\">(EP..) ([^<]+)<\/a>!i", $line, $matches)) {
+                $airports[]['url'] = $matches[1];
+                $airports[]['icao'] = $matches[2];
+                $airports[]['name'] = $matches[3];
             }
+        }
 
-            $chart->icao = $d[ICAO];
-            $chart->airportname = utf8_encode($d[AP_NAME]);
-            $chart->country = "PL";
-            $chart->chartname = utf8_encode($d[CHART_NAME]);
-            if ($d[CHART_TYPE] == "Arrival") {
-                $chart->charttype = "STAR";
-            } elseif ($d[CHART_TYPE] == "Approach") {
-                $chart->charttype = "Approach";
-            } elseif ($d[CHART_TYPE] == "Departure") {
-                $chart->charttype = "SID";
-            } else {
-                $chart->charttype = "General";
+        foreach($airports as $airport) {
+            $chart_url = "";
+            $chart_name = "";
+            $data = file($this->index, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+            foreach ($data as $line) {
+                if (preg_match("!href=\'download\.php?ts.+&file=(ead/EP_AD_.+\.pdf)\' class=\'aip\'!i", $line, $matches)) {
+                    $chart_url = $matches[1];
+                }
+                if (preg_match("!\s+>(.+)<\/a>!", $line, $matches) && $chart_url) {
+                    $chart_name = $matches[1];
+                    $chart = Chart::where('icao', $airport['icao'])->where('chartname', $chart_name)->first();
+                    if (!$chart) { $chart = new Chart(); }
+                    $chart->icao = $airport['icao'];
+                    $chart->airportname = utf8_encode($airport['name']);
+                    $chart->country = "PL";
+                    $chart->chartname = $chart_name;
+                    if (preg_match("!SID!", $chart_name)) {
+                        $chart->charttype = "SID";
+                    } elseif (preg_match("!STAR!", $chart_name)) {
+                        $chart->charttype = "STAR";
+                    } elseif (preg_match("!(GNSS|NDB|ILS|VOR|Visual)!", $chart_name)) {
+                        $chart->charttype = "Approach";
+                    } else {
+                        $chart->charttype = "General";
+                    }
+                    $chart->id = sha1("pl." . $airport['icao'] . ",." . $chart_name);
+                    $chart->url = $this->chart_base . $chart_url;
+                    $chart->save();
+                }
             }
-            $chart->url = $d[CHART_URL];
-            $chart->id = sha1("pl." . $chart->icao . ",." . $d[CHART_NAME]);
-            $chart->save();
         }
 
         // Clear out non-updated charts
