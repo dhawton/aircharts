abc5f0ba4209313e867eb3037290b592d6b505c4
Daniel Hawton <daniel@hawton.com>
2017-04-24 01:44:55 -0800

    Fix routing tables

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
-Route::get('/', 'ACController@getIndex');
-Route::post('/charts','ACController@postCharts');
-Route::post('/deploy', function() {
-    exec("cd /home/airchar1/ac2/aircharts3 && git pull");
+Route::group(['domain' => 'www.aircharts.org'], function () {
+    Route::get('/', 'ACController@getIndex');
+
+    Route::post('/charts', 'ACController@postCharts');
+    Route::post('/deploy', function () {
+        exec("cd /home/airchar1/ac2/aircharts3 && git pull");
+    });
 });
 
-Route::group(['middleware' => 'api', 'domain' => 'api.aircharts.org'], function() {
+Route::group(['middleware' => 'api', 'domain' => 'api.aircharts.org'], function () {
+    Route::get('/', function() { echo "Coming soon"; });
     Route::get('/Airport/{data}', 'APIController@getAirport');
 });
