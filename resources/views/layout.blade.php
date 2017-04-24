6eff058aa1edcd7856629a2f5f3f0264fbaa8641
Daniel Hawton <daniel@hawton.com>
2017-04-24 01:11:49 -0800

    Port to regular bootstrap

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,35 +2,46 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="/css/app.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
     <title>AirCharts</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-<nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle Nav">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+    </div>
 
     <a class="navbar-brand" href="/">AirCharts</a>
 
     <div class="collapse navbar-collapse" id="navbar">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
+        <ul class="nav navbar-nav">
+            <li>
                 <a href="/" class="nav-link">Home</a>
             </li>
-            <li class="nav-item">
+            <li>
                 <a href="/about" class="nav-link">About</a>
             </li>
-            <li class="nav-item">
+            <li>
                 <a href="//api.aircharts.org" class="nav-link">API</a>
             </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0" method="post" action="/charts">
+        <form class="navbar-form navbar-right" method="post" action="/charts">
             {{ csrf_field() }}
-            <input class="form-control mr-sm-2" type="text" placeholder="Chart Search" name="query">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="searchbtn">Search</button>
+            <div class="form-group">
+                <input class="form-control" type="text" placeholder="Chart Search" name="query">
+            </div>
+            <button class="btn btn-success" type="submit" id="searchbtn">Search</button>
         </form>
     </div>
 </nav>
@@ -64,8 +75,7 @@
 @yield('content')
 
 <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 @yield('js')
 </body>
 </html>
