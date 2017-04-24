9b0f7727530657bccd7652002751e0552a610ceb
Daniel Hawton <daniel@hawton.com>
2017-04-23 23:31:59 -0800

    Working regexp

--- app/Console/Commands/SpiderUK.php
+++ app/Console/Commands/SpiderUK.php
@@ -50,12 +50,11 @@ class SpiderUK extends Command
                 $icao = $matches[3];
                 $airportfile = file($url, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES); $airporturl = $url;
                 foreach ($airportfile as $line) {
-                    if (/*preg_match("!class=\"desc\" width=\"55%\"><a target=\"_blank\" href=\"([^\"]+)\">(.+)\s*<\/a>!", $line, $matches) ||*/
-                        preg_match("!class=\"desc\"[A-Z 0-9a-z\%\"]*><a target=\"_blank\" href=\"([^\"]+)\">(.+)\s*<\/a>!", $line, $matches)) {
+                    if (preg_match("!class=\"desc\"[^>]*><a target=\"_blank\" href=\"([^\"]+)\">(.+)<\/a>!", $line, $matches)) {
                         echo "$icao - " . $matches[2] . " - '" . $airporturl . "'\n";
                         if ($x == 10) { exit; }
                         $charturl = $matches[1];
-                        $chartname = $matches[2];
+                        $chartname = trim($matches[2]);
                         $charttype = "General";
                         if (preg_match("!INSTRUMENT APPROACH (CHART|PROCEDURE|PROCEDURES) (.+)$!", $chartname, $matches)) {
                             $charttype = "Approach";
