bd752133da84da45b233c65e442edbf53824f6fa
Daniel Hawton <daniel@hawton.com>
2017-04-24 00:47:54 -0800

    Add webhook code

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,6 @@
 
 Route::get('/', 'ACController@getIndex');
 Route::post('/charts','ACController@postCharts');
+Route::post('/deploy', function() {
+    exec("cd /home/airchar1/ac2/aircharts3 && git pull");
+});
