ee7a0a049fbe2b1167d3c9c86f8fd5e79c61c9d9
Daniel Hawton <daniel@hawton.com>
2017-04-24 00:43:39 -0800

    Fix CSS

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="/css/app.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/app.css" crossorigin="anonymous">
 
     <title>AirCharts</title>
     <meta charset="utf-8">
