dce9813a3f54815bb10a021f0d948ea2fb0f33e1
Daniel Hawton <daniel@hawton.com>
2017-04-24 10:37:37 -0800

    spider:fr regexp fix

--- app/Console/Commands/SpiderFR.php
+++ app/Console/Commands/SpiderFR.php
@@ -44,7 +44,7 @@ class SpiderFR extends Command
         $data = file($this->vatfrurl, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
         $in_charts = 0;
         foreach ($data as $line) {
-            if (preg_match("!id=\"charts\"", $line)) { $in_charts = 1; }
+            if (preg_match("!id=\"charts\"!", $line)) { $in_charts = 1; }
             if (preg_match("!href=\"([^\"]+\/(LF..)\/AD 2 LF.. ([^\"]+)\.pdf)\"!", $line, $matches) && $in_charts) {
                 $url = $matches[1];
                 $icao = $matches[2];
@@ -56,15 +56,15 @@ class SpiderFR extends Command
                 $chart->country = "FR";
                 $chart->url = $url;
 
-                if (preg_match("SID", $chartname)) {
+                if (preg_match("!SID!", $chartname)) {
                     $chart->charttype = "Departure";
                     $chartname = str_replace("SID ", "", $chartname);
                 }
-                elseif (preg_match("STAR", $chartname)) {
+                elseif (preg_match("!STAR!", $chartname)) {
                     $chart->charttype = "Approach";
                     $chartname = str_replace("STAR ", "", $chartname);
                 }
-                elseif (preg_match("IAC", $chartname)) {
+                elseif (preg_match("!IAC!", $chartname)) {
                     $chart->charttype = "Approach";
                     $chartname = str_replace("IAC ", "", $chartname);
                 }
