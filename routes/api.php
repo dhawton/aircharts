648e8b1b4ec42d3a04a92843497f38afd07139e9
Daniel Hawton <daniel@hawton.com>
2017-04-24 01:40:09 -0800

    Move API to web routes since we use a subdomain

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,3 @@
 <?php
 
 use Illuminate\Http\Request;
-
-Route::group(['middleware' => 'api', 'domain' => 'api.aircharts.org'], function() {
-    Route::get('/Airport/{data}', 'APIController@getAirport');
-});
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ Route::post('/charts','ACController@postCharts');
 Route::post('/deploy', function() {
     exec("cd /home/airchar1/ac2/aircharts3 && git pull");
 });
+
+Route::group(['middleware' => 'api', 'domain' => 'api.aircharts.org'], function() {
+    Route::get('/Airport/{data}', 'APIController@getAirport');
+});
