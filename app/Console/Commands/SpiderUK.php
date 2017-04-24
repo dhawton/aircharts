0e2ff69eafe1befd3be4378da9bee2f2a74f6206
Daniel Hawton <daniel@hawton.com>
2017-04-24 01:05:28 -0800

    Fix UK Spider

--- app/Console/Commands/SpiderUK.php
+++ app/Console/Commands/SpiderUK.php
@@ -41,6 +41,16 @@ class SpiderUK extends Command
      */
     public function handle()
     {
+        $header = [
+            "Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5",
+            "Cache-Control: max-age=0",
+            "Connection: keep-alive",
+            "Keep-Alive: 300",
+            "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7",
+            "Accept-Language: en-us,en;q=0.5",
+            "Pragma: "
+        ];
+
         $x = 0; // For testing purposes, only do ~10 charts.
         $index = file($this->base_url . $this->index_url, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
         foreach ($index as $index_line) {
@@ -48,7 +58,8 @@ class SpiderUK extends Command
                 $url = str_replace("&amp;", "&", $this->base_url . $matches[1]);
                 $name = $matches[2];
                 $icao = $matches[3];
-                $airportfile = file($url, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES); $airporturl = $url;
+                $airportfile = file($url, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+                $airporturl = $url;
                 foreach ($airportfile as $line) {
                     if (preg_match("!class=\"desc\"[^>]*><a target=\"_blank\" href=\"([^\"]+)\">(.+)<\/a>!", $line, $matches)) {
                         $charturl = $matches[1];
@@ -81,14 +92,6 @@ class SpiderUK extends Command
                         $chart->chartname = $chartname;
                         $chart->charttype = $charttype;
 
-                        $header[] = "Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
-                        $header[] = "Cache-Control: max-age=0";
-                        $header[] = "Connection: keep-alive";
-                        $header[] = "Keep-Alive: 300";
-                        $header[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
-                        $header[] = "Accept-Language: en-us,en;q=0.5";
-                        $header[] = "Pragma: "; // browsers keep this blank.
-
                         $ch = curl_init();
                         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
                         curl_setopt($ch, CURLOPT_VERBOSE, false);
@@ -99,10 +102,10 @@ class SpiderUK extends Command
                         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
                         curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate,sdch');
                         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-                        $result=curl_exec($ch);
+                        $result = curl_exec($ch);
 
                         \Storage::disk('s3')->put("uk/" . $chart->id . ".pdf", $result, "public");
-                        sleep(1);
+                        //sleep(1);
                         $chart->url = "http://awsir.aircharts.org/uk/" . $chart->id . ".pdf";
                         $chart->save();
                     }
