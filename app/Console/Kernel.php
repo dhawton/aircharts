a0a06e27b2138b28d636ee69b7658acc25badb61
Daniel Hawton <daniel@hawton.com>
2017-04-24 11:19:34 -0800

    spider:at

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\UpdateVATSIM::class,
+        Commands\SpiderAT::class,
         Commands\SpiderDE::class,
         Commands\SpiderFR::class,
         Commands\SpiderUK::class,
