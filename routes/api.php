4a1b5a26c5ef8d73fc59fd96b55ddf3d5e810b44
Daniel Hawton <daniel@hawton.com>
2017-04-24 01:36:56 -0800

    fix api routes

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Http\Request;
 
 Route::group(['middleware' => 'api', 'domain' => 'api.aircharts.org'], function() {
-    Route::get('Airport/{data}', 'APIController@getAirport');
+    Route::get('/Airport/{data}', 'APIController@getAirport');
 });
