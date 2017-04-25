f6e06ab7559da3559daf3c361ad1db1f7b747aaa
Daniel Hawton <daniel@hawton.com>
2017-04-24 17:05:48 -0800

    Use non-slim jquery

--- resources/views/ECR/layout.blade.php
+++ resources/views/ECR/layout.blade.php
@@ -12,7 +12,7 @@
 </head>
 <body>
 @yield('content')
-<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"
         integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"
         crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
