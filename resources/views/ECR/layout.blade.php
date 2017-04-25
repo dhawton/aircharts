1764c4d86dd6e50f80d0a2882af5fbba1e636e8d
Daniel Hawton <daniel@hawton.com>
2017-04-24 17:06:53 -0800

    remove integrity checks

--- resources/views/ECR/layout.blade.php
+++ resources/views/ECR/layout.blade.php
@@ -13,10 +13,8 @@
 <body>
 @yield('content')
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"
-        integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"
         crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
         crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
 @yield('js')
