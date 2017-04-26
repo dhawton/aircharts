50c60fdd2a247e5c77869fe135a93cfc12630c91
Daniel Hawton <daniel@hawton.com>
2017-04-25 16:27:04 -0800

    Tweak JS

--- public/js/ecr.js
+++ public/js/ecr.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
             dataType: 'json',
             success: function (data) {
                 $.each(data, function() {
-                    if (typeof this == object) {
+                    if (typeof this == "object") {
                         $('#airportinfo').html(this.info.id + " - " + this.info.name);
                         var html = "";
                         $.each(this.General, function () {
