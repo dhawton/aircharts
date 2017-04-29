51ca3e00076cf8e24972936d737cd243284fee11
Daniel Hawton <daniel@hawton.com>
2017-04-28 18:29:02 -0800

    Add API Charts cmd

--- app/Http/Controllers/APIv2Controller.php
+++ app/Http/Controllers/APIv2Controller.php
@@ -8,13 +8,14 @@ use Illuminate\Http\Request;
 
 class APIv2Controller extends Controller
 {
-    public function getAirport($data) {
+    public function getAirport($data)
+    {
         $data = str_replace(" ", "", $data);
         if (preg_match("![^A-Z,]+!", $data)) {
             return response()->json(['status' => 'error', 'msg' => 'Malformed Request'], 400);
         }
 
-        if (preg_match('!,!',$data)) {
+        if (preg_match('!,!', $data)) {
             $aps = explode(",", $data);
         } else {
             $aps[] = $data;
@@ -23,7 +24,7 @@ class APIv2Controller extends Controller
         $output = [
         ];
         $error = 0;
-        foreach($aps as $ap) {
+        foreach ($aps as $ap) {
             $airport = Airport::where('id', $ap)->first();
             if (!$airport) {
                 $output[$ap] = "Not Found";
@@ -37,13 +38,13 @@ class APIv2Controller extends Controller
                 'longitude' => $airport->lon,
                 'elevation' => $airport->elevation
             ];
-            $groups = ['General','SID','STAR','Intermediate','Approach'];
-            foreach($groups as $group) {
+            $groups = ['General', 'SID', 'STAR', 'Intermediate', 'Approach'];
+            foreach ($groups as $group) {
                 $charts = Chart::where(function ($query) use ($ap) {
                     $query->where('icao', $ap);
 //                    $query->orWhere('iata', $ap);
-                })->where('charttype',$group)->orderBy('chartname')->get();
-                foreach($charts as $chart) {
+                })->where('charttype', $group)->orderBy('chartname')->get();
+                foreach ($charts as $chart) {
                     $output[$ap]["charts"][$group][] = [
                         'id' => $chart->id,
                         'chartname' => $chart->chartname,
@@ -56,6 +57,40 @@ class APIv2Controller extends Controller
 
         if (count($aps) == 1 && $error) {
             return response()->json($output, 404);
+        }
+
+        return response()->json($output);
+    }
+
+    public function getCharts($ap)
+    {
+        $output = [
+        ];
+        $error = 0;
+        $airport = Airport::where('id', $ap)->first();
+        if (!$airport) {
+            return response()->json(["error" => "Not Found"],404);
+            }
+        $output['info'] = [
+            'id' => $ap,
+            'name' => $airport->name,
+            'latitude' => $airport->lat,
+            'longitude' => $airport->lon,
+            'elevation' => $airport->elevation
+        ];
+        $groups = ['General', 'SID', 'STAR', 'Intermediate', 'Approach'];
+        foreach ($groups as $group) {
+            $charts = Chart::where(function ($query) use ($ap) {
+                $query->where('icao', $ap);
+            })->where('charttype', $group)->orderBy('chartname')->get();
+            foreach ($charts as $chart) {
+                $output[$ap]["charts"][$group][] = [
+                    'id' => $chart->id,
+                    'chartname' => $chart->chartname,
+                    'url' => $chart->url,
+                    'proxy' => "https://www.aircharts.org/view/" . $chart->id
+                ];
+            }
         }
 
         return response()->json($output);
